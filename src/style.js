4a54d82dc9098b19fe8ce0764ddfea5847fe3465
Andrea <darkpunisher.andrea@gmail.com>
2021-09-01 14:18:31 +0200

    Added in JS

--- src/style.js
+++ src/style.js
@@ -22,6 +22,31 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#weather");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+            <div class="col-2">
+                <div class="forecast-date">${day}</div>
+                <img src="http://openweathermap.org/img/wn/50d@2x.png"
+                alt=""
+                width="40" />
+                <div class="forecast-temp">
+                    <span class="forecast-temp-max">18&#176</span>
+                    <span class="forecast-temp-min">12&#176</span>
+                    </div>
+                </div>
+            `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
